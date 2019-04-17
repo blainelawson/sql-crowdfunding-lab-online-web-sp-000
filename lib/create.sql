1baddbef81a2d2995f77a0e3f45561e8477f219d
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-17 17:08:58 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE project (
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  category TEXT,
+  funding INTEGER,
+  start_date INTEGER,
+  end_date INTEGER 
+);
+
+CREATE TABLE user (
+  name TEXT,
+  age INTEGER
+);
